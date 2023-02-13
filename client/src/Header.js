2eee7cf49a94457f8b06cdc3f73d1a733da8b787
Mark Agluba <mark.agluba@dawsoncollege.qc.ca>
2023-02-13 12:09:32 -0500

    making a header for the home page

---
+++ client/src/Header.js
@@ -0,0 +1,14 @@
+function Header() {
+  return(
+    <>
+      <div className="title">
+        WORDICT
+      </div>
+      <div className="description">
+        Whatever the description is
+      </div>
+    </>
+  );
+}
+
+export default Header;
