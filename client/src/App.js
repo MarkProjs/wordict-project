c35c30ffc5b60d14112af71eb59c49d29b56a90a
Mark Agluba <mark.agluba@dawsoncollege.qc.ca>
2023-02-13 12:24:41 -0500

    adding a navbar but nothing in it

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from "./Header.js";
+import NavBar from "./NavBar.js";
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <NavBar/>
 
       
     </div>
---
+++ client/src/NavBar.js
@@ -0,0 +1,16 @@
+function NavBar() {
+  return(
+    <>
+      <ul>
+        <li>Home</li>
+        <li>Dictionary</li>
+        <li>Play Wordle!</li>
+        <li>Play Wordle Online</li>
+        <li>LeaderBoard</li>
+        <li>About!</li>
+      </ul>
+    </>
+  );
+}
+
+export default NavBar;
