c2fa181a04faf1b12a71df0fe8aee58c4e7b10ba
Nolan Ganz <nolanganz1@gmail.com>
2023-02-15 18:43:53 -0500

    Make function to style letters

--- client/src/components/WordRow.js
+++ client/src/components/WordRow.js
@@ -1,5 +1,6 @@
 import Letter from "./Letter.js";
 import {useEffect, useRef} from "react";
+import { RIGHT, HALF_RIGHT, WRONG } from "../controllers/GameLogic.js";
 import "./WordRow.css"
 
 function WordRow(props) {
@@ -10,17 +11,38 @@ function WordRow(props) {
     console.log(letter); 
     console.log(currentLetter.current);
     if (letter === props.deleteKey && currentLetter.current > 0) {
-      stateFuncs.current[--currentLetter.current](props.defaultValue);
+      stateFuncs.current[--currentLetter.current]((prev) => {
+        return {...prev, key: props.defaultValue};
+      });
       props.letters[currentLetter.current] = props.defaultValue;
     } else if (letter !== props.deleteKey && currentLetter.current < props.wordLength) {
-      stateFuncs.current[currentLetter.current](letter);
+      stateFuncs.current[currentLetter.current]((prev) => {
+        return {...prev, key: letter};
+      });
       props.letters[currentLetter.current] = letter;
       currentLetter.current++;
     }
   }
 
+  function applyLetterStyles(styles) {
+    styles.forEach((styleNum, index) => {
+      let style = "";
+      if (styleNum === RIGHT) {
+        style = "right"
+      } else if (styleNum === HALF_RIGHT) {
+        style = "half-right"
+      } else {
+        style = "wrong";
+      }
+      stateFuncs.current[index.valueOf()]((prev) => {
+        return {...prev, style: style}
+      });
+    });
+  }
+
   useEffect(() => {
     props.addInputListener(updateLetters);
+    props.addStyleListener(applyLetterStyles);
   }, []);
 
   function addLetter(stateFunc) {
