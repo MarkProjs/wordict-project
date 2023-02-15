750b467f81c937e1cb43fa77468a7c725c45ec69
unknown <jeremy.tang@dawsoncollege.qc.ca>
2023-02-15 11:59:18 -0500

    clean up code

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -1,48 +1,22 @@
-import React, {useState} from 'react'
+import {useState} from 'react'
 
-
-const SearchBar = () => {
+function SearchBar() {
 
   const [searchInput, setSearchInput] = useState("");
-
-  const words = [
-    { word: "hello", definition: "a greeting" },
-    { word: "draw", definition: "to draw" },
-    { word: "definition", definition: "meaning of a word" },
-  ];
 
   const handleChange = (e) => {
     e.preventDefault();
     setSearchInput(e.target.value);
-    console.log(searchInput);
   };
 
-  // if (searchInput.length > 0) {
-  //   words.filter((word) => {
-  //     if (word.word === searchInput){
-  //       return true;
-  //     } else {
-  //       return false;
-  //     }
-  //   });
-  // }
-
   return <div>
-
     <input
       type="search"
       placeholder="Search here"
       onChange={handleChange}
       value={searchInput} />    
-
-    {/* {words.map((word, index) => {
-      console.log(searchInput);
-      console.log("in map: " + word.word);
-    })} */}
-
   </div>
 
-
-};
+}
 
 export default SearchBar;
