67070eb920a62cc5119b57d35bc99b6985032319
Nolan Ganz <nolanganz1@gmail.com>
2023-02-15 18:39:00 -0500

    Update naming of constants to reflect what they are

--- client/src/controllers/GameLogic.js
+++ client/src/controllers/GameLogic.js
@@ -1,8 +1,8 @@
 import validInput from "./ValidInput.json"
 
-const GREEN = 0;
-const YELLOW = 1;
-const RED = 2;
+const RIGHT = 0;
+const HALF_RIGHT = 1;
+const WRONG = 2;
 
 function validateInput(input) {
   return validInput.validInput.includes(input)
@@ -12,24 +12,24 @@ function checkSubmission(submission, word) {
   const result = [];
   submission.split("").forEach((letter, index, array) => {
     if (letter === word.charAt(index)) {
-      result.push(GREEN);
+      result.push(RIGHT);
     } else {
       let nextOccurence = word.indexOf(letter, index);
       if (nextOccurence === -1) {
         let prevOccurence = word.indexOf(letter)
-        if (prevOccurence !== -1 && result[prevOccurence.valueOf()] !== GREEN) {
-          result.push(YELLOW);
+        if (prevOccurence !== -1 && result[prevOccurence.valueOf()] !== RIGHT) {
+          result.push(HALF_RIGHT);
         } else {
-          result.push(RED);
+          result.push(WRONG);
         }
       } else if (array[nextOccurence.valueOf()] !== letter) {
-        result.push(YELLOW);
+        result.push(HALF_RIGHT);
       } else {
-        result.push(RED);
+        result.push(WRONG);
       }
     }
   });
   return result;
 }
 
-export {validateInput, checkSubmission};
+export {validateInput, checkSubmission, RIGHT, HALF_RIGHT, WRONG};
