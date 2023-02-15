2056b99d884692530b82b5f28b1752bd3345be20
Mark Agluba <mark.agluba@dawsoncollege.qc.ca>
2023-02-15 11:47:41 -0500

    Adding the Home component in App to see in screen

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from "./components/Header.js";
 import NavBar from "./components/NavBar.js";
+import Home from "./components/Home.js";
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <NavBar/>
+      <Home/>
 
       
     </div>
