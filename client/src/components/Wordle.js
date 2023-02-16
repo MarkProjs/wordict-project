43e6ed07fe8bfc3918438e7e56cd14676d20018e
Nolan Ganz <nolanganz1@gmail.com>
2023-02-16 10:40:48 -0500

    Refactor code for readability

--- client/src/components/Wordle.js
+++ client/src/components/Wordle.js
@@ -3,40 +3,70 @@ import * as GameLogic from "../controllers/GameLogic.js";
 import { useEffect, useRef } from "react";
 
 function Wordle(props) {
-  const keyEventFuncs = useRef([]);
-  const styleEventFuncs = useRef([]);
+
+  // Contains all of the functions subscribed to the key event
+  const keyEvent = useRef([]);
+
+  // Contains all of the functions subscribed to the style event
+  const styleEvent = useRef([]);
+
   const currentRow = useRef(0);
+
+  // Set the default value of the letters to be an array of default values
   const letters = useRef(new Array(props.word.length).fill(props.defaultValue));
 
-  function addInputListener(listenerFunc) {
-    keyEventFuncs.current.push(listenerFunc);
+  /**
+   * Subscribe a function to the key event
+   * @param {Function} subFunc The function to subscribe to the key event
+   */
+  function subToKeyEvent(subFunc) {
+    keyEvent.current.push(subFunc);
   }
 
-  function addStyleListener(styleFunc) {
-    styleEventFuncs.current.push(styleFunc);
+
+  /**
+   * Subscribe a function to the style event
+   * @param {Function} subFunc The function to subscribe to the style event
+   */
+  function subToStyleEvent(subFunc) {
+    styleEvent.current.push(subFunc);
   }
 
+  /**
+   * Process keyboard input and trigger game events accordingly
+   * @param {Event} e The key event
+   * @param {Function} send An optional function to send the key event result somewhere else
+   */
   function handleInput(e, send = undefined) {
     let key = e.key.toLocaleUpperCase();
+
     //validate key
     if (GameLogic.validateInput(key)) {
-      console.log(letters.current)
-      console.log(letters.current.every(letter => console.log(letter)))
+      
+      // Only submit if their entire word is filled 
+      // TODO add check if it is a valid word
       if (
         key === props.submitKey 
         && letters.current.every(letter => letter !== props.defaultValue)
       ) {
-        //Submit game logic code stuff here
-        console.log(letters.current.join(""));
+        // Get the result array to determine which letters are correct
         let results = GameLogic.checkSubmission(letters.current.join(""), props.word);
-        styleEventFuncs.current[currentRow.current](results);
-        currentRow.current = currentRow.current + 1 >= props.attempts ? 0 : currentRow.current + 1;
-        console.log("Enter " + currentRow.current);
+
+        // Trigger the style event for the current row
+        styleEvent.current[currentRow.current](results);
+
+        currentRow.current++;
+
+        //clear the current word that is being written
         letters.current.forEach((letter, index, array) => {
           array[index.toFixed()] = props.defaultValue;
         })
       } else if (key !== props.submitKey) {
-        keyEventFuncs.current[currentRow.current](key);
+
+        // Trigger the key event for the current row
+        keyEvent.current[currentRow.current](key);
+        
+        // If the key should be sent somewhere else send it here
         send?.call(key);
       }
     }
@@ -44,6 +74,7 @@ function Wordle(props) {
   }
 
   useEffect(() => {
+    // Get keyboard input from the parent component
     props.addInputListener(handleInput);
   }, []);
   
@@ -54,8 +85,8 @@ function Wordle(props) {
         return <WordRow 
           key={index} 
           wordLength={props.word.length} 
-          addInputListener={addInputListener}
-          addStyleListener={addStyleListener}
+          subToKeyEvent={subToKeyEvent}
+          subToStyleEvent={subToStyleEvent}
           letters={letters.current}
           deleteKey={props.deleteKey}
           defaultValue={props.defaultValue}
