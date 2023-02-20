416ae1ed42525fec703dfd7e44b4813d2718b8e7
unknown <jeremy.tang@dawsoncollege.qc.ca>
2023-02-20 12:08:46 -0500

    add comments and tests for dictionary get api

--- server/__tests__/api.test.js
+++ server/__tests__/api.test.js
@@ -1,6 +1,7 @@
 import app from "../app.js";
 import request from "supertest";
 
+// tests for definition get api
 describe('GET /api/monkey/definition', () => {
   // success
   test('/api/monkey/definition', async () => {
@@ -11,5 +12,20 @@ describe('GET /api/monkey/definition', () => {
   test('fail /api/monkey/definition', async () => {
     const response = await request(app).get('/api/monkey/definition');
     expect(response.body.word).not.toEqual("gorilla");
+  })
+});
+
+// tests for dictionary get api
+describe('GET /api/dictionary', () => {
+  // success
+  test('/api/dictionary', async () => {
+    const response = await request(app).get('/api/dictionary');
+    // this is using mock data and is subject to change
+    expect(response.body[2].word).toEqual("stick"); 
+  })
+  // fail
+  test('fail /api/dictionary', async () => {
+    const response = await request(app).get('/api/dictionary');
+    expect(response.body[0].word).not.toEqual("gorilla");
   })
 });
