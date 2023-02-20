691398ee85d7327cbc49ea8b74d034c686d0b54f
unknown <jeremy.tang@dawsoncollege.qc.ca>
2023-02-20 10:49:15 -0500

    clean up code

--- server/routes/api.js
+++ server/routes/api.js
@@ -6,7 +6,7 @@ const router = express.Router();
  * Get API to retrieve Definition
  */
 router.get("/:word/definition", async (req, res) => {
-  const word = req.query.word
+  // const word = req.query.word
 
   let data = {"word": "monkey"};
   // Retrieve data from MongoDB
@@ -14,22 +14,13 @@ router.get("/:word/definition", async (req, res) => {
   res.json(data);
 })
 
-// at top level
-let words;
-/* try {
-  const db = new DB(); // controller.js, not db
-  words = db.getAllWords();
-} catch (error) {
-  console.error(error);
-}*/
-
 /**
  * Get API to retrieve Dictionary
  */
 router.get("/dictionary", async (req, res) => {
-  words = [{"word": "monkey"}, {"word": "evolution"}, {"word": "stick"}, {"word": "rock"}];
+  let words = [{"word": "monkey"}, {"word": "evolution"}, {"word": "stick"}, {"word": "rock"}];
   // Retrieve data from MongoDB
-
+  // let words = db.getAllWords();
   res.json(words);
 })
 
