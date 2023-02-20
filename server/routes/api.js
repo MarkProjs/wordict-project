97cccfdcd0a793212a28ff9ef4f7c458df4da998
unknown <jeremy.tang@dawsoncollege.qc.ca>
2023-02-20 12:01:56 -0500

    add optional query param as comment

--- server/routes/api.js
+++ server/routes/api.js
@@ -17,8 +17,13 @@ router.get("/:word/definition", async (req, res) => {
 /**
  * Get API to retrieve Dictionary
  */
+// router.get("/dictionary/:length?", async (req, res) => {
 router.get("/dictionary", async (req, res) => {
   let words = [{"word": "monkey"}, {"word": "evolution"}, {"word": "stick"}, {"word": "rock"}];
+  
+  // if (req.query.word !== undefined){
+  //   words = words.filter(word => word.word.length >= req.query.word.length);
+  // }
   // Retrieve data from MongoDB
   // let words = db.getAllWords();
   res.json(words);
