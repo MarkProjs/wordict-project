bb6e1f230e58c3028f43937e9ba90f67ba044eca
unknown <jeremy.tang@dawsoncollege.qc.ca>
2023-02-20 11:01:45 -0500

    move search bar to dictionary

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 import Header from "./components/Header.js";
-import SearchBar from './components/SearchBar';
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      <SearchBar />
     </div>
   );
 }
--- client/src/components/pages/Dict.js
+++ client/src/components/pages/Dict.js
@@ -1,7 +1,10 @@
+import SearchBar from './components/SearchBar';
+
 function Dict() {
   return(
     <>
       <h1>Dictionary</h1>
+      <SearchBar />
     </>
   );
 }
