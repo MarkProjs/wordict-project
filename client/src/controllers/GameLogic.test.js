2252d3fbb1d3e2e1fd80a36b566834c3e4f58bec
Nolan Ganz <nolanganz1@gmail.com>
2023-02-21 15:49:10 -0500

    Test GameLogic

--- client/src/controllers/GameLogic.test.js
+++ client/src/controllers/GameLogic.test.js
@@ -20,6 +20,16 @@ describe("Word check tests", () => {
     ]
     expect(GameLogic.checkSubmission("hello", "hello")).toEqual(expected);
   });
+  test("All Right Different Capitalisation", () => {
+    let expected = [
+      GameLogic.RIGHT, 
+      GameLogic.RIGHT, 
+      GameLogic.RIGHT, 
+      GameLogic.RIGHT, 
+      GameLogic.RIGHT
+    ]
+    expect(GameLogic.checkSubmission("HElLo", "heLlO")).toEqual(expected);
+  });
   test("All Wrong", () => {
     let expected = [
       GameLogic.WRONG, 
@@ -29,6 +39,16 @@ describe("Word check tests", () => {
       GameLogic.WRONG
     ]
     expect(GameLogic.checkSubmission("waist", "hello")).toEqual(expected);
+  });
+  test("All Wrong Different Capitalisation", () => {
+    let expected = [
+      GameLogic.WRONG, 
+      GameLogic.WRONG, 
+      GameLogic.WRONG, 
+      GameLogic.WRONG, 
+      GameLogic.WRONG
+    ]
+    expect(GameLogic.checkSubmission("WaIsT", "hello")).toEqual(expected);
   });
   test("All Half Right", () => {
     let expected = [
@@ -40,4 +60,28 @@ describe("Word check tests", () => {
     ]
     expect(GameLogic.checkSubmission("lleoh", "hello")).toEqual(expected);
   });
+  test("All Half Right Different Capitalisation", () => {
+    let expected = [
+      GameLogic.HALF_RIGHT, 
+      GameLogic.HALF_RIGHT, 
+      GameLogic.HALF_RIGHT, 
+      GameLogic.HALF_RIGHT, 
+      GameLogic.HALF_RIGHT
+    ]
+    expect(GameLogic.checkSubmission("LLeoh", "HEllo")).toEqual(expected);
+  });
+  test("No Duplicate Half Right", () => {
+    let expected = [
+      GameLogic.HALF_RIGHT, 
+      GameLogic.HALF_RIGHT, 
+      GameLogic.HALF_RIGHT, 
+      GameLogic.HALF_RIGHT, 
+      GameLogic.WRONG, 
+      GameLogic.WRONG
+    ]
+    expect(GameLogic.checkSubmission("eppeep", "people")).toEqual(expected);
+  })
+  test("Invalid Submission", () => {
+    expect(() => GameLogic.checkSubmission(undefined, "hello")).toThrow(new Error("Invalid inputs"))
+  })
 });
