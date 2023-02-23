508bce2854e0a1d346c46fcd1106be6fd96d726a
Mark Agluba <mark.agluba@dawsoncollege.qc.ca>
2023-02-23 13:25:47 -0500

    adding the Error page in case someone tried to access a non-existant page

--- client/src/components/pages/Error.js
+++ client/src/components/pages/Error.js
@@ -1,7 +1,8 @@
 function Error() {
   return (
     <>
-      <h1>ERROR! You tried to access a non-existent page. Go Back!</h1>
+      <h1>Error 404!</h1>
+      <h2>You tried to access a non-existent page. Go Back!</h2>
     </>
   );
 }
