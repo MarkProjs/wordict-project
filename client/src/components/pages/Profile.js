64aee04e148029ef2648fc8343fdcbc2fc7c5e87
unknown <jeremy.tang@dawsoncollege.qc.ca>
2023-02-23 13:12:07 -0500

    add image and name to profile page

--- client/src/components/pages/Profile.js
+++ client/src/components/pages/Profile.js
@@ -1,6 +1,12 @@
-function Profile(){
+function Profile(props){
   return (
-    <h1>Profile</h1>
+    <main>
+      <h1>My Profile</h1>
+      <section>
+        <img style={{width: 100, height: 100}} src={props.image}/>
+        <p>{props.name}</p>
+      </section>
+    </main>
   )
 }
 
