34e4ebad6d4c7db47190d94011908ca315f31b1c
unknown <jeremy.tang@dawsoncollege.qc.ca>
2023-02-27 12:30:55 -0500

    add components to the profile page

--- client/src/components/pages/Profile.js
+++ client/src/components/pages/Profile.js
@@ -1,10 +1,26 @@
-function Profile(props){
+// most likely going to remove props
+function Profile(props) {
+  const mockData = [{ word: 'one' }, { word: 'happy' }, { word: 'rooty' }];
+
+  // fetch image, username, favorite words from database
+
   return (
     <main>
       <h1>My Profile</h1>
-      <section>
-        <img style={{width: 100, height: 100}} src={props.image}/>
+      <section className="profile">
+        <img style={{ width: 100, height: 100 }} src={props.image} />
         <p id="username">{props.name}</p>
+      </section>
+      <section className="favorites">
+        <ul>
+          {mockData.map((item, key) =>
+            <li key={key}>
+              <a href={"/" + item.word + "/definition"}>{item.word}</a>
+            </li>)}
+        </ul>
+      </section>
+      <section className="rank">
+        <p>Leaderboard rank: #1</p>
       </section>
     </main>
   )
