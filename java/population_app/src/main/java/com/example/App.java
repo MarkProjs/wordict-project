9dce8a542b067e294f05e996bedb2317a7a2a32f
Grigor Mihaylov <gori3303@gmail.com>
2023-02-26 19:25:24 -0500

    untils done

--- java/population_app/src/main/java/com/example/App.java
+++ java/population_app/src/main/java/com/example/App.java
@@ -2,11 +2,19 @@ package com.example;
 
 import com.example.data_structs.Definition;
 import com.example.data_structs.Word;
+import com.example.data_structs.WordsDictionary;
+import com.example.utils.CSVinterpreter;
+import com.example.utils.MongoUtil;
 import com.mongodb.*;
 import com.mongodb.client.*;
 import org.bson.*;
 import org.bson.types.*;
+
+import java.io.IOException;
 import java.util.*;
+
+import javax.swing.plaf.synth.SynthTextAreaUI;
+
 import org.bson.codecs.pojo.*;
 import org.bson.codecs.configuration.*;        
 import io.github.cdimascio.dotenv.Dotenv;
@@ -20,33 +28,40 @@ import io.github.cdimascio.dotenv.Dotenv;
  */
 public class App 
 {
-    public static void main( String[] args )
+    
+    public static void main( String[] args ) throws IOException
     {
+        CSVinterpreter interpreter;
+        interpreter = new CSVinterpreter("C:\\Users\\14388\\Documents\\School\\W2023\\wordict\\dictionary.csv");
+        System.out.println("FILE LOADED");
 
-        Dotenv dotenv = Dotenv.load();
-        String url = dotenv.get("ATLAS_URI");
-        System.out.println(url);
-        ConnectionString connectionString = new ConnectionString(url);
-        CodecRegistry pojoCodecRegistry = CodecRegistries.fromProviders(PojoCodecProvider.builder().automatic(true).build());
-        CodecRegistry codecRegistry = CodecRegistries.fromRegistries(MongoClientSettings.getDefaultCodecRegistry(),pojoCodecRegistry);
-        MongoClientSettings clientSettings = MongoClientSettings.builder().applyConnectionString(connectionString).codecRegistry(codecRegistry).build();       
-        MongoClient client = MongoClients.create(clientSettings);
+        String[][] data = interpreter.getData();
+        WordsDictionary dict = new WordsDictionary();
+        };
+
+        for(int i = 0; i < data.length; i++){
+            if()
+        }
         
 
-        MongoDatabase database = client.getDatabase("test");
-        MongoCollection<Word> words = database.getCollection("wordsv3", Word.class);
+        //saveToDB();
 
-        Definition d = new Definition("n.","Something");
-        List<Definition> def = new ArrayList<Definition>();
-        def.add(d);
-        List<String> r = new ArrayList<String>();
-        List<String> s = new ArrayList<String>();
-        Word w1 = new Word("Hello", 5, def, r, s);
+    }
 
-        words.insertOne(w1);
+    private static void saveToDB(ArrayList<Word> words){
+        //setup
+        Dotenv dotenv = Dotenv.load();
+        String uri = dotenv.get("ATLAS_URI");
+        MongoUtil util = new MongoUtil(uri);
+        util.connectClient();
+        util.connectDB("test");
+        util.connectCollection("wordsv2", Word.class);
+        System.out.println("CONNECTED TO DB");
 
-        Word w = words.find().first();
-        System.out.println(w.getWord());
+        //DO STUFF
+        MongoCollection<Word> client = (MongoCollection<Word>) util.getCollection();
+        client.insertMany(words);
+        System.out.println("SAVED");
 
     }
 }
--- java/population_app/src/main/java/com/example/utils/CSVinterpreter.java
+++ java/population_app/src/main/java/com/example/utils/CSVinterpreter.java
@@ -1,7 +1,41 @@
 package com.example.utils;
 
-
+import java.nio.file.*;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.List;
 
 public class CSVinterpreter {
-    private Mongo
+    private String[][] data;
+    private String filePath;
+
+    public CSVinterpreter(String filePath) throws IOException{
+        this.filePath = filePath;
+        load();
+    }
+    private void load() throws IOException{
+      List<String> rawData = Files.readAllLines(Paths.get(this.filePath), StandardCharsets.UTF_8);
+      int rows = rawData.size();
+      int columns = rawData.get(0).split(",").length;
+      data = new String[rows][columns];
+      for(int i = 0; i < rawData.size(); i++){
+        String[] colms = rawData.get(i).split(",");
+        data[i] = colms;
+      }
+    }
+    public String[][] getData(){
+        return this.data;
+    }
+    public int getNumberOfRows(){
+        return this.data.length;
+    }
+    public int getNumberOfColumns(){
+        return this.data[0].length;
+    }
+    public String getAt(int row, int column){
+        return data[row][column];
+    }
+    
 }
