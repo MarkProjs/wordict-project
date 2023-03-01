7eb85e8fb51dd6cee21974843bf12a8696b13c80
Nolan Ganz <nolanganz1@gmail.com>
2023-03-01 12:38:16 -0500

    Setup router to route to multiplayer page

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,7 +3,8 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home.js";
 import Dict from "./pages/Dict.js";
 import Error from "./pages/Error.js";
-import SinglePlayerWordle from './pages/SinglePlayerWordle.js';
+import SinglePlayerWordle from "./pages/SinglePlayerWordle.js";
+import MultiPlayerWordle from "./pages/MultiPlayerWordle.js"
 
 function Header() {
   return(
@@ -17,6 +18,7 @@ function Header() {
           <Route path="/" exact element={<Home/>} />
           <Route path="/dict" exact element={<Dict/>} />
           <Route path="/wordle" exact element={<SinglePlayerWordle/>} />
+          <Route path="/wordle-online" exact element={<MultiPlayerWordle/>} />
           <Route path="/*" exact element= {<Error/>}/>
         </Routes>
       </Router>
