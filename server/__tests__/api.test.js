cb6969d766c6ac808a6a3ad1ecc295c9fd15af2d
Jacky Tat <tatjacky123@gmail.com>
2023-03-02 13:30:12 -0500

    added lcov html based report

--- server/__tests__/api.test.js
+++ server/__tests__/api.test.js
@@ -30,15 +30,15 @@ controllers.getAllWords = jest.fn((e) => {
 })
 
 // tests for definition get api
-describe('GET /api/three/definition', () => {
+describe('Test Definition GET API', () => {
   // test for existing word
-  test('/api/monkey/definition', async () => {
+  test('Test for existing word', async () => {
     const response = await request(app).get('/api/monkey/definition');
     expect(response.body.word).toEqual("monkey");
     expect(response.body.definitions).toBeDefined()
   })
   // test for non existant word
-  test('fail /api/asdasd/definition', async () => {
+  test('Test for non existant word', async () => {
     const response = await request(app).get('/api/asdasd/definition');
     expect(response.body.word).not.toBeDefined();
     expect(response.body.word).not.toEqual("monkey");
@@ -46,15 +46,15 @@ describe('GET /api/three/definition', () => {
 });
 
 // tests for dictionary get api
-describe('GET /api/dictionary', () => {
+describe('Test Dictionary GET API', () => {
   // test for all words
-  test('/api/dictionary', async () => {
+  test('Test Retrieving all words', async () => {
     const response = await request(app).get('/api/dictionary');
     expect(response.body).toContain("elite");
     expect(response.body).not.toContain("daousbofia");
   })
   // test for specific length
-  test('/api/dictionary?length=5', async () => {
+  test('Test for retrieving all words of Specific length', async () => {
     const response = await request(app).get('/api/dictionary?length=5');
     expect(response.body).toContain("three");
     expect(response.body).not.toContain("present");
