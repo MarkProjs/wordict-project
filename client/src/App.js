7a0b6a505a509dd44eab62be406d0866a4c408f0
Mark Agluba <mark.agluba@dawsoncollege.qc.ca>
2023-03-02 13:37:24 -0500

    adding a google auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,29 @@
 import './App.css';
 import Header from "./components/Header.js";
+import { GoogleLogin } from '@react-oauth/google';
 
 function App() {
+  const handleLogin = async googleData =>{
+    const res = await fetch("/auth", {
+      method: "POST",
+      body: JSON.stringify({token: googleData.credential}),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+    const data = await res.json()
+  }
 
+  const handleError = error => {
+    console.error(error);
+  }
   return (
     <div className="App">
       <Header/>
+      <GoogleLogin
+        onSuccess={handleLogin}
+        onError={handleError} 
+      />
     </div>
   );
 }
