16a86e6e1c7a3e0e128d4837dd76d1139e3212ea
Nolan Ganz <nolanganz1@gmail.com>
2023-03-05 13:35:00 -0500

    add definition to word popup and which person you are

--- client/src/components/Wordle/Popup.js
+++ client/src/components/Wordle/Popup.js
@@ -3,17 +3,36 @@ import "./Popup.css";
 
 function Popup(props) {
   const [gameState, setGameState] = useState({done: false, win: false});
-
+  const [definitions, setDefinitions] = useState([]);
   useEffect(() => {
     // Subscribe to the parent events
     props.subToGameStateEvent(props.id, setGameState);
   });
 
+  useEffect(() => {
+    (async () => {
+      let url = new URL(`/api/${props.word.toLowerCase()}/definition`, location.origin);
+      let data;
+      try {
+        let response = await fetch(url);
+        data = await response.json();
+      } catch (e) {
+        data = {definitions: []};
+      }
+      setDefinitions(data.definitions);
+    })();
+  }, [props.word]);
+
   return (
     <>
       {gameState.done ? <article className="popup">
-        <p>You have {gameState.win ? "won!!!" : "Lost :(("}</p>
+        <p>{props.person} have {gameState.win ? "won!!!" : "Lost :(("}</p>
         <p>The word was {props.word}</p>
+        <ul>
+          {definitions.map((definition, i) => {
+            return <li key={i}>{definition.definition}</li>
+          })}
+        </ul>
       </article> : <></>}
     </>
   );
