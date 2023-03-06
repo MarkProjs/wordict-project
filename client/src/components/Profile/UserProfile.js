fe29a5c1fca326fdc873f5b4c57aefa51267b773
unknown <jeremy.tang@dawsoncollege.qc.ca>
2023-03-06 12:38:33 -0500

    add props to UserProfile

--- client/src/components/Profile/UserProfile.js
+++ client/src/components/Profile/UserProfile.js
@@ -1,10 +1,8 @@
-function UserProfile() {
-  let mockImage = "https://discovery.sndimg.com/content/dam/images/discovery/fullset/2021/4/30/" +
-    "GettyImages-1189192456.jpg.rend.hgtvcom.406.406.suffix/1619849704543.jpeg";
+function UserProfile(props) {
   // might need props to receive image and username, or fetch from db here
   return (
     <article className="profile">
-      <img src={mockImage} />
+      <img src={props.image} alt="user profile picture" />
     </article>
   );
 }
--- client/src/components/pages/Profile.js
+++ client/src/components/pages/Profile.js
@@ -6,11 +6,13 @@ import './Profile.css';
 function Profile() {
   // fetch image, username, favorite words from database
   let mockName = "MonkeyMan420";
+  let mockImage = "https://discovery.sndimg.com/content/dam/images/discovery/fullset/2021/4/30/" +
+  "GettyImages-1189192456.jpg.rend.hgtvcom.406.406.suffix/1619849704543.jpeg";
 
   return (
     <main>
       <section className="left-section">
-        <UserProfile />
+        <UserProfile image={mockImage}/>
         <UserRank />
       </section>
       <section className="right-section">
