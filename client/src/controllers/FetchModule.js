f391bcf2c3336c3809db492dfb4934c9c8534bdd
Nolan Ganz <nolanganz1@gmail.com>
2023-03-07 19:22:48 -0500

    Add function to get all words of a specified length

--- client/src/controllers/FetchModule.js
+++ client/src/controllers/FetchModule.js
@@ -11,7 +11,23 @@ async function fetchDefinition(word) {
   return data;
 }
 
+async function fetchAllWords(length = undefined) {
+  let words;
+  let url = new URL(`/api/dictionary`, location.origin);
+  if (length) {
+    url.searchParams.set("length", length);
+  }
+  try {
+    let response = await fetch(url);
+    words = await response.json();
+  } catch(e) {
+    words = [];
+  }
+  return words;
+}
+
 export default {
   fetchDefinition,
-
+  fetchAllWords,
+  
 }
