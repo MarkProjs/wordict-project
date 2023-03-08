a592104c7d6bc104568abc81436ba8887336e655
unknown <jeremy.tang@dawsoncollege.qc.ca>
2023-03-08 12:47:53 -0500

    add profile edit toggle functionality

--- client/src/components/pages/Profile.js
+++ client/src/components/pages/Profile.js
@@ -1,27 +1,42 @@
+import { useState } from 'react';
 import FavoriteWords from "../Profile/FavoriteWords";
 import UserProfile from "../Profile/UserProfile";
 import UserRank from "../Profile/UserRank";
 import './Profile.css';
 
 function Profile() {
+  const [isViewMode, setIsViewMode] = useState(true);
   // fetch image, username, favorite words from database
   let mockName = "MonkeyMan420";
   let mockImage = "https://discovery.sndimg.com/content/dam/images/discovery/fullset/2021/4/30/" +
-  "GettyImages-1189192456.jpg.rend.hgtvcom.406.406.suffix/1619849704543.jpeg";
+    "GettyImages-1189192456.jpg.rend.hgtvcom.406.406.suffix/1619849704543.jpeg";
 
-  return (
-    <main>
+  const profileView =
+    <>
       <section className="left-section">
-        <UserProfile image={mockImage}/>
+        <UserProfile image={mockImage} />
         <UserRank />
       </section>
       <section className="right-section">
         <div className="top-part">
           <h1 id="username">{mockName}</h1>
-          <button>Edit profile</button>
+          <button onClick={() => setIsViewMode(false)}>Edit profile</button>
         </div>
         <FavoriteWords />
       </section>
+    </>;
+
+  const profileEdit =
+    <>
+      <h1>
+        EDIT
+      </h1>
+      <button onClick={() => setIsViewMode(true)}>Save profile</button>
+    </>;
+
+  return (
+    <main>
+      {isViewMode ? profileView : profileEdit}
     </main>
   )
 }
