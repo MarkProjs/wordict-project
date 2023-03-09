5fdd8a5ef1458e1e7c59cf228c9888ff88fc818f
unknown <jeremy.tang@dawsoncollege.qc.ca>
2023-03-09 16:32:18 -0500

    add form to edit profile

--- client/src/components/pages/Profile.js
+++ client/src/components/pages/Profile.js
@@ -10,11 +10,12 @@ function Profile() {
   let mockName = "MonkeyMan420";
   let mockImage = "https://discovery.sndimg.com/content/dam/images/discovery/fullset/2021/4/30/" +
     "GettyImages-1189192456.jpg.rend.hgtvcom.406.406.suffix/1619849704543.jpeg";
+  const [profilePicture, setProfilePicture] = useState(mockImage);
 
   const profileView =
     <>
       <section className="left-section">
-        <UserProfile image={mockImage} />
+        <UserProfile image={profilePicture} />
         <UserRank />
       </section>
       <section className="right-section">
@@ -27,12 +28,30 @@ function Profile() {
     </>;
 
   const profileEdit =
-    <>
-      <h1>
-        EDIT
-      </h1>
-      <button onClick={() => setIsViewMode(true)}>Save profile</button>
-    </>;
+    <form onSubmit={updateProfile} className="edit-form">
+      <h2>
+        Edit Profile
+      </h2>
+      <label>Name: </label>
+      <input id="username" type="text" name="username" defaultValue={mockName} />
+      <br />
+      <fieldset className="picture-section">
+        <label>Change profile picture: </label>
+        <input type="file" name="file" accept="image/*"
+          onChange={(e) => setProfilePicture(window.URL.createObjectURL(e.target.files[0]))} />
+        <p>Preview:</p>
+        <img alt="profile picture" src={profilePicture}/>
+      </fieldset>
+      <br />
+      <input type="submit" value="Save" className="form-buttons"/>
+      <button className="form-buttons" onClick={() => setIsViewMode(true)}>Cancel</button>
+    </form>;
+
+  function updateProfile() {
+    // update user profile using api
+
+    setIsViewMode(true);
+  }
 
   return (
     <main>
