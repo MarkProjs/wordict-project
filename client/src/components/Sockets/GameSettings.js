5dfa0921caa6477576dafa5bb39db97bdfef7948
Nolan Ganz <nolanganz1@gmail.com>
2023-03-10 18:19:28 -0500

    Add validity check to word to send

--- client/src/components/Sockets/GameSettings.js
+++ client/src/components/Sockets/GameSettings.js
@@ -3,25 +3,41 @@ import { useState } from "react";
 function GameSettings(props) {
 
   const [word, setWord] = useState("");
+  const [isWordValid, setIsWordValid] = useState(true);
 
   function randomiseWord(e) {
     e.preventDefault();
-    console.log(props)
     let wordNum = Math.floor(Math.random() * props.allWords.current.length);
     setWord(props.allWords.current[wordNum.valueOf()]);
+    e.target.parentNode.word.setCustomValidity("");
   }
 
   function handleSend(e) {
     e.preventDefault();
-    console.log(word);
+    console.log("testtest " + word)
+    if (props.allWords.current.includes(word)) {
+      setIsWordValid(true); 
+      console.log(word);
+    } else {
+      setIsWordValid(false);
+      e.target.word.setCustomValidity("That word is not in our dictionary");
+      e.target.word.reportValidity();
+    }
+
   }
 
   return (
-    <form onSubmit={e => handleSend(e)}>
-      <label htmlFor="word">Opponent&apos;s Word</label>
-      <input value={word} onInput={(e) => setWord(e.target.value)} name="word"/>
+    <form onSubmit={e => {handleSend(e)
+      console.log("SUBMIT")
+    }}>
+      <label htmlFor="word">Opponent&apos;s Word: </label>
+      <input required 
+        value={word} onInput={(e) => {setWord(e.target.value)
+          e.target.setCustomValidity("");
+        }} name="word"/>
       <button>Send</button>
-      <button onClick={e => randomiseWord(e)}>Send Random</button>
+      <br/>
+      <button onClick={e => randomiseWord(e)}>Randomise</button>
     </form>
   );
 }
