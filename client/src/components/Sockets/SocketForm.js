9f067f7c56985626fc76fb2f63537604b9e13c29
Nolan Ganz <nolanganz1@gmail.com>
2023-03-19 15:13:41 -0400

    Add socket initialisation to form

--- client/src/components/Sockets/SocketForm.js
+++ client/src/components/Sockets/SocketForm.js
@@ -1,27 +1,77 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import SocketContext from "./SocketContext";
+import io from "socket.io-client";
 
-function SocketForm(props) {
+function SocketForm() {
   
+  const [isConnected, setIsConnected] = useState(false);
   const [currentRoom, setCurrentRoom] = useState("");
+  const socketContext = useContext(SocketContext);
+  const navigate = useNavigate();
 
+  // Attempt to connect on click, also setup listeners on the socket
+  function initialiseSocket(room) {
+    // If a connection is open, close it
+    if (socketContext.socket.current) {
+      socketContext.socket.current.disconnect();
+    }
 
-  function handleFormSubmit(e) {
-    e.preventDefault();
-    props.initialiseSocket(e.target.room.value);
-    props.socket.current.on("return-code", (message) => {
+    // Open a new connection
+    socketContext.socket.current = io("", { query: { room: room } });
+
+    // Show the text area upon connecting
+    socketContext.socket.current.on("connect", () => {
+      console.log("Connected");
+      setIsConnected(true);
+    });
+
+    // Remove the text field if there is a disconnect
+    socketContext.socket.current.on("disconnect", (reason) => {
+      console.log("disconnect");
+      console.log(reason);
+    });
+
+    // Log the error if there is one
+    socketContext.socket.current.on("connect_error", (error) => {
+      console.log(error);
+    });
+
+    // Set the lobby to full so the pregame setup can start
+    socketContext.socket.current.on("lobby-full", () => {
+      navigate("/wordle-online/startup", {replace: true});
+    });
+
+    // Display the room code for easy invites
+    socketContext.socket.current.on("return-code", (message) => {
       console.log(message);
       setCurrentRoom(message.code);
-    })
+    });
+  }
+
+  /**
+   * When the user tries to connect, setup the socket
+   * @param {Event} e Submit event for the connection form
+   */
+  function handleFormSubmit(e) {
+    e.preventDefault();
+    initialiseSocket(e.target.room.value);
   }
 
   return (
-    <form onSubmit={(e) => handleFormSubmit(e)}>
-      <p>Your Room: {currentRoom}</p>
-      <input id="nextRoom" name="room"/>
-      <button>
-        Connect
-      </button>
-    </form>
+    <>
+      <form onSubmit={(e) => handleFormSubmit(e)}>
+        <p>Your Room: {currentRoom}</p>
+        <input id="nextRoom" name="room"/>
+        <button>
+          Connect
+        </button>
+      </form>
+      {
+        isConnected && <div>Waiting For Players</div>
+        || <div>Connect To Start</div>
+      }
+    </>
   );
 }
 
