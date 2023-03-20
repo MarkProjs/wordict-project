31f015cb6da453246d09b2b5c8d5295146610b1e
Jacky Tat <tatjacky123@gmail.com>
2023-03-20 12:40:33 -0400

    added leaderboard page

---
+++ client/src/components/pages/Leaderboard.js
@@ -0,0 +1,21 @@
+import './Leaderboard.css';
+
+function Leaderboard() {
+  return <>
+    <h1>Leaderboard</h1>
+    <table>
+      <tr>
+        <th>Rank</th>
+        <th>Player</th>
+        <th>Points</th>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>Your mom</td>
+        <td>420</td>
+      </tr>
+    </table>
+  </>
+}
+
+export default Leaderboard;
