e2c60935755ff738470f0b81728638a0a7891156
Mark Agluba <mark.agluba@dawsoncollege.qc.ca>
2023-03-27 12:28:57 -0400

    trying to move the GoogleOAuthProvider tag

--- client/src/components/pages/LogIn.js
+++ client/src/components/pages/LogIn.js
@@ -1,20 +1,18 @@
-import { GoogleLogin, GoogleOAuthProvider } from "@react-oauth/google";
+import { GoogleLogin } from "@react-oauth/google";
 function Login(props) {
   return(
     <div className="login">
       <h1>You are not logged in!</h1>
       <p>You can log in via: </p>
-      <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
-        {!props.userName && 
-          <GoogleLogin 
-            onSuccess={props.handleLogin}
-            onError={()=>{
-              console.log('Login Failed');
-            }}
-            cookiePolicy={"single_host_origin"}
-          />
-        }
-      </GoogleOAuthProvider>
+      {!props.userName && 
+        <GoogleLogin 
+          onSuccess={props.handleLogin}
+          onError={()=>{
+            console.log('Login Failed');
+          }}
+          cookiePolicy={"single_host_origin"}
+        />
+      }
     </div>
   );
 }
