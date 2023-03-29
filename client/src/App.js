277494775ca7146dc08ffa91f4d7d083af684027
Nolan Ganz <nolanganz1@gmail.com>
2023-03-28 20:31:29 -0400

    Let entire app have access to previous page context

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import Header from "./components/Header.js";
+import PreviousPageContext from './components/NavigationExtra/PreviousPageContext';
 
 function App() {
 
   return (
     <div className="App">
-      <Header/>
+      <PreviousPageContext.Provider value={{}}>
+        <Header/>
+      </PreviousPageContext.Provider>
     </div>
   );
 }
