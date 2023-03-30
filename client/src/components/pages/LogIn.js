4d8d87e1f6d9dbf8defe235d7ce5812dd90a0adc
Mark Agluba <mark.agluba@dawsoncollege.qc.ca>
2023-03-30 12:05:38 -0400

    adding the context in the login

--- client/src/components/pages/LogIn.js
+++ client/src/components/pages/LogIn.js
@@ -1,14 +1,16 @@
 import { GoogleLogin, GoogleOAuthProvider } from "@react-oauth/google";
 import "./Login.css";
-
+import userContext from "../../userContext.js";
+import { useContext } from 'react';
 function Login(props) {
+  const user = useContext(userContext);
   return(
     <div className="login">
       <h1>You are not logged in!</h1>
       <p>You can log in via: </p>
       <div>
         <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
-          {!props.userName && 
+          {!user.username && 
             <GoogleLogin 
               onSuccess={props.handleLogin}
               onError={()=>{
